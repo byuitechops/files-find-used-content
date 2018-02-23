7efd063479404f60a0870ff2920f96f5f133fa3c
Stewart <maeashley@byui.edu>
2018-02-23 14:01:01 -0700

    working properly, update dependencies

--- main.js
+++ main.js
@@ -2,44 +2,59 @@
 
 const pathLib = require('path'),
     URL = require('url');
-//need to update function comments and documentation
 /*Take a course object and return a list (array) of content pages that are used in the course. */
 module.exports = (course, stepCallback) => {
     var $;
+    //eventually use the 'reorganize file strucure repo for this except DO NOT replace codeFiles
     var docExts = ['.doc', '.docx', '.pdf', '.xls', '.xlsx', '.csv', '.odt', '.ods', '.txt', '.dat', '.log', '.mdb', '.sav', '.sql', '.tar',
             '.xlr', '.wpd', '.wks', '.wps', '.xlsm', '.rtf', '.xps', '.ppt', '.pptx', '.pps', '.slk',
         ],
         imgExts = ['.png', '.jpeg', '.gif', '.bmp', '.ai', '.ico', '.jpg', '.ps', '.psd', '.svg', '.tif', '.tiff'],
         audVidExts = ['.avi', '.wmv', '.mpg', '.mpeg', '.swf', '.mov', '.mp4', '.aif', '.cda', '.mid', '.midi', '.mp3', '.wav', '.ogg',
             '.wma', '.wpl'
-        ];
-    /*convert to paths*/
+        ],
+        codeFiles = ['.css', '.js', '.xml'];
+    /*helperFunction: convert to paths*/
     function htmlFileObjToPath(htmlFileObj) {
         return htmlFileObj.path;
     }
-    /*filter to html*/
+    /*helperFunction: filter to html*/
     function toHtml(path) {
         return pathLib.extname(path) === '.html';
     }
-    /*filter to unique*/
+    /*helperFunction: filter to unique*/
     function toUnique(path, i, filteredArray) {
         return filteredArray.indexOf(path) === i;
     }
-    /*filter to hrefs from an obj*/
+    /*helperFunction: filter to hrefs from an obj*/
     function toHrefCheerio(i, resource) {
         return $(resource).attr('href');
     }
-    /*filter out quicklinks, they don't need to be accounted for */
+    /*helperFunction:filter to internal links*/
+    function toInternal(url) {
+        var myUrl = URL.parse(url);
+        return myUrl.hostname === null;
+    }
+    /*helperFunction: decode %20s*/
+    function decodeMe(htmlFilepath) {
+        return decodeURI(htmlFilepath);
+    }
+    /*helperFunction: filter out quicklinks*/
     function noQuicklink(path) {
-        if (!path.includes('quickLink')) {
-            return path;
-        }
+        return !path.includes('quickLink');
     }
     var start = 1;
 
     function printRound(n) {
         start = start + n;
         console.log('Starting Round #', start);
+    }
+    //helperFunction: for getManifestHtmlFilepaths
+    function getManifest() {
+        var manifest = course.content.find(function (file) {
+            return file.name === 'imsmanifest.xml';
+        });
+        return manifest;
     }
     /*sorts hrefs to get used content, not just html filepaths */
     function sortHrefs(hrefs) {
@@ -48,34 +63,26 @@ module.exports = (course, stepCallback) => {
                 name = pathLib.basename(href);
             if (imgExts.includes(ext)) {
                 courseImages.push({
-                    name: href,
-                    ext: name
+                    name: name,
+                    path: href
                 });
             } else if (docExts.includes(ext)) {
                 courseDocs.push({
-                    name: href,
-                    ext: name
+                    name: name,
+                    path: href
                 });
             } else if (audVidExts.includes(ext)) {
                 courseVids.push({
-                    name: href,
-                    path: name
+                    name: name,
+                    path: href
                 });
             } else if (ext !== '.html') {
-                course.warning('file type not recognized ' + href);
+                course.warning('file type not recognized ' + href + 'EXTENSION' + ext);
             }
         });
     }
-    //helper function for getManifestHtmlFilepaths
-    function getManifest() {
-        var manifest = course.content.find(function (file) {
-            return file.name === 'imsmanifest.xml';
-        });
-        return manifest;
-    }
 
     //1. Get List of absolute filepaths in the manifest
-    //remove course as a parameter, may need to add it back in
     function getManifestHtmlFilePaths() {
         var manifest = getManifest();
         $ = manifest.dom;
@@ -85,6 +92,7 @@ module.exports = (course, stepCallback) => {
             //unfiltered resources should 
             .filter(toHtml)
             .filter(toUnique);
+        printRound(0);
         return resources;
     }
 
@@ -115,16 +123,11 @@ module.exports = (course, stepCallback) => {
                 return htmlLinksOut.concat(htmlFileObj.dom('a').map(toHrefCheerio).get());
             }, [])
             .filter(toUnique)
-            //filter to internal links
-            .filter(function (url) {
-                var myUrl = URL.parse(url);
-                return myUrl.hostname === null;
-            })
+            .filter(toInternal)
+            //eventually  need to take this html filter off, because css, js, and xml files can be re-crawled.
             .filter(toHtml)
-            //map href BACK to array of HtmlFilepaths
-            .map(function (htmlFilepath) {
-                return decodeURI(htmlFilepath);
-            });
+            //map href BACK to array of parsed hrefs
+            .map(decodeMe);
         //retrieve image tags and add them to courseImages
         var imageTags = arrayofHtmlFileObjs.reduce(function (imgsOut, fileObj) {
                 return imgsOut.concat(fileObj.dom('img').get());
@@ -146,8 +149,8 @@ module.exports = (course, stepCallback) => {
             .filter(function (url) {
                 var myUrl = URL.parse(url);
                 return myUrl.hostname === null;
-            })
-            .filter(toUnique)
+            }).map(decodeMe)
+            .filter(toInternal)
             .filter(noQuicklink);
         sortHrefs(courseFiles);
         return filteredHtmlFilepathStrings;
@@ -174,11 +177,8 @@ module.exports = (course, stepCallback) => {
             moreHtmlFilepaths,
             filteredListOfFileObjs;
 
-        //2. Convert HTML filepaths into a cheerio parsed html file objects
+        //2. Convert HTML filepaths into html file objects
         htmlFilepathObjs = htmlFilepathsToHtmlFileObjs(course, fplist);
-
-        //3.Record the paths we found
-        usedHtmlFilepaths = getKnownFilepaths(htmlFilepathObjs);
 
         //4. Find more HTML filepaths that are linked to from the other HTML DOMs
         moreHtmlFilepaths = findMoreHtmlFilepaths(htmlFilepathObjs);
@@ -186,31 +186,20 @@ module.exports = (course, stepCallback) => {
         //5. Remove HTML filepaths from newly found html filepath strings that are known
         filteredListOfFileObjs = removeKnownFilepaths(moreHtmlFilepaths, usedHtmlFilepaths);
 
+        //3.Record the paths we found
+        usedHtmlFilepaths = getKnownFilepaths(htmlFilepathObjs);
+
         if (filteredListOfFileObjs.length > 0) {
             printRound(1);
             //because there are new filepaths, crawl that content for more html filepaths
             usedHtmlFilepaths = usedHtmlFilepaths
                 .concat(crawlContent(course, filteredListOfFileObjs))
-                //make the list unique
                 .filter(toUnique);
         } else {
             course.message('No more extra filepaths were found.');
         }
         return usedHtmlFilepaths;
     }
-    //1. Get List of absolute filepaths in the manifest
-    var fplist = getManifestHtmlFilePaths(course),
-        usedHtmlFilepaths = [],
-        filteredHtmlFilepathStrings,
-        courseImages = [],
-        courseDocs = [],
-        courseVids = [],
-        nonUsedFiles;
-
-    //start crawling content
-    usedHtmlFilepaths = crawlContent(course, fplist);
-    // wrapItUp(usedHtmlFilepaths);
-    wrapItUp(usedHtmlFilepaths);
 
     function wrapItUp(usedHtmlFilepaths) {
         var allFiles = usedHtmlFilepaths.map(function (fp) {
@@ -222,24 +211,22 @@ module.exports = (course, stepCallback) => {
         });
         //put all the used images, docs and audio/video file objects to allFiles
         allFiles = allFiles.concat(courseImages, courseDocs, courseVids);
-        var codeFilesExts = ['.xml', '.js', '.css'];
-
         nonUsedFiles = course.content.filter(function (file) {
             var ext = file.ext;
-            //don't enter the checking phase if it is a code file as well as don't keep it in nonusedFiles
-            if (codeFilesExts.includes(ext)) {
+            //if its a codeFile don't enter the checking phase and don't keep it in nonusedFiles
+            if (codeFiles.includes(ext)) {
                 return false;
             }
-            //this checking phase needs to work
-            for (var i = 0; i < allFiles.length - 1; i++) {
-                if (allFiles[i].name === file.name) {
-                    return false;
-                }
-            }
+            //return first value on allFiles that matches the current file we're looping over
+            var match = allFiles.find(function (usedFile) {
+                return usedFile.name === file.name;
+            });
+            return !match;
+
         });
-        // console.log('USED', allFiles);
-        // console.log('UNUSED', nonUsedFiles);
-        // debugger;
+        if (nonUsedFiles.length === 0) {
+            course.message('There are no unused files.');
+        }
         nonUsedFiles.forEach(unusedFile => {
             course.log('Unused Files', {
                 'Name': unusedFile.name,
@@ -248,7 +235,7 @@ module.exports = (course, stepCallback) => {
         });
         allFiles.forEach(usedFile => {
             if (!usedFile.path) {
-                console.log(course.warning('this file does not have a path property: ' + usedFile));
+                course.warning(usedFile + ' does not have a path property.');
             }
             course.log('Used Files', {
                 'Name': usedFile.name,
@@ -256,6 +243,20 @@ module.exports = (course, stepCallback) => {
             });
         });
     }
+
+    //1. Get List of absolute filepaths in the manifest
+    var fplist = getManifestHtmlFilePaths(course),
+        usedHtmlFilepaths = [],
+        filteredHtmlFilepathStrings,
+        courseImages = [],
+        courseDocs = [],
+        courseVids = [],
+        nonUsedFiles;
+
+    //crawl content
+    usedHtmlFilepaths = crawlContent(course, fplist);
+    wrapItUp(usedHtmlFilepaths);
+
     nonUsedFiles = nonUsedFiles.map(file => file.name);
     course.newInfo('usedFiles', usedHtmlFilepaths);
     course.newInfo('nonUsedFiles', nonUsedFiles);
